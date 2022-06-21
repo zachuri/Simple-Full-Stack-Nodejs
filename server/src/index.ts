390850f12bbe30a002cf42bf0034712681eb0323
zachuri <punsalang.zachary@gmail.com>
2022-06-20 22:51:07 -0700

    setup graphql/apollo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,46 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+// import { AppDataSource } from "./data-source";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
-AppDataSource.initialize().then(async () => {
+(async () => {
+	const app = express();
+	app.get("/", (_req, res) => res.send("hello"));
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+	const apolloSever = new ApolloServer({
+		typeDefs: `
+      type Query {
+        hello: String!
+      }
+    `,
+		resolvers: {
+			Query: {
+				hello: () => "hello world",
+			},
+		},
+	});
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+  await apolloSever.start();
+	apolloSever.applyMiddleware({ app });
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+	app.listen(4000, () => {
+		console.log("express server started");
+	});
+})();
 
-}).catch(error => console.log(error))
+// AppDataSource.initialize().then(async () => {
+
+//     console.log("Inserting a new user into the database...")
+//     const user = new User()
+//     user.firstName = "Timber"
+//     user.lastName = "Saw"
+//     user.age = 25
+//     await AppDataSource.manager.save(user)
+//     console.log("Saved a new user with id: " + user.id)
+
+//     console.log("Loading users from the database...")
+//     const users = await AppDataSource.manager.find(User)
+//     console.log("Loaded users: ", users)
+
+//     console.log("Here you can setup and run express / fastify / any other framework.")
+
+// }).catch(error => console.log(error))
